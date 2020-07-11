70e6aa07ce661253dee4dc14c8c502766bffd940
Hubitskiy <dmitry.g@upservice.io>
2020-07-11 21:10:03 +0300

    activation

--- applications/users/urls.py
+++ applications/users/urls.py
@@ -1,7 +1,8 @@
 from django.urls import path
-from .views import CreateUserView
+from .views import CreateUserView, ActivateUserView
 
 
 urlpatterns = [
     path('create/', CreateUserView.as_view()),
+    path('activate/', ActivateUserView.as_view({'post': 'create'}))
 ]
